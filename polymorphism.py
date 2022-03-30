7c33965c9800bce13977913d4fb7e5b70fe4afa5
ayse.kaya <ayssekyaa@gmail.com>
2022-03-31 01:58:40 +0300

    polymorphism practice.

---
+++ polymorphism.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 01:51:33 2022
+
+@author: ayyse
+"""
+
+class Employee:
+    def raisee(self):
+        raise_rate=0.1
+        result=100+100*raise_rate
+        print("Employee: ",result)
+        
+        
+class CompEng(Employee):
+    def raisee(self):
+        raise_rate=0.2
+        result=100+100*raise_rate
+        print("CompEng: ",result)
+        
+        
+class EEE(Employee):
+    def raisee(self):
+        raise_rate=0.3
+        result=100+100*raise_rate
+        print("EEE: ",result)
+        
+e1=Employee()
+ce=CompEng()
+eee=EEE()
+
+employee_list=[ce,eee]
+
+for employee in employee_list:
+    employee.raisee()
